1f7bfc401b803af63995bec01ab0b401ae77164e
dkgell13 <dkgell13@gmail.com>
2023-07-19 22:08:09 +0900

    new : 초기 설정

---
+++ src/main/java/kr/co/galaxy/authn/AuthnApplication.java
@@ -0,0 +1,13 @@
+package kr.co.galaxy.authn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthnApplication.class, args);
+    }
+
+}
