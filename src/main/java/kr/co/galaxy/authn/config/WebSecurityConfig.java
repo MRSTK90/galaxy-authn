9a302af116e3e278ac9c3d55ab639b32c72ba966
kst <kst@osstem.com>
2023-10-19 20:21:29 +0900

    new : create security class

---
+++ src/main/java/kr/co/galaxy/authn/config/WebSecurityConfig.java
@@ -0,0 +1,22 @@
+package kr.co.galaxy.authn.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/**")
+                        .permitAll())
+                .build();
+    }
+}
---
+++ src/main/java/kr/co/galaxy/authn/filter/LoginFilter.java
@@ -0,0 +1,8 @@
+package kr.co.galaxy.authn.filter;
+
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+public class LoginFilter extends UsernamePasswordAuthenticationFilter {
+
+
+}
